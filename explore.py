96ac79903836c046029a9807b56f3670639ee541
simpleParadox <abomicode@gmail.com>
2022-01-22 14:21:36 -0700

    first commit

---
+++ explore.py
@@ -0,0 +1,3 @@
+import json
+f = open("G:\cmput_656_project\dataset\dataverse_files\\award.award_nominated_work.award_nominations..award.award_nomination.award_nominee.json")
+award_temp = json.load(f)
